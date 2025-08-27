6fb8b4900b602a1b2686e81d33a4821b8c741a73
mauromltn <mauromtne@icloud.com>
2025-08-27 22:58:19 +0200

    feat: Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Home Gourmet",
+  description: "Porta l’esperienza del ristorante a casa tua",
+};
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="it">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}>
+        {children}
+      </body>
+    </html>
+  );
+}
