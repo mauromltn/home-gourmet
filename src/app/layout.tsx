b160d782dca813b4cac66620b9e3b9a75780acdd
mauromltn <mauromtne@icloud.com>
2025-08-28 03:22:07 +0200

    feat: Create Hero section with example images and CTA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter, Playfair_Display } from "next/font/google"
+import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
+  display: "swap",
+  variable: "--font-inter",
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const playfair = Playfair_Display({
   subsets: ["latin"],
-});
+  display: "swap",
+  variable: "--font-playfair",
+})
 
 export const metadata: Metadata = {
   title: "Home Gourmet",
@@ -20,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="it">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}>
+      <body className={`${inter.variable} ${playfair.variable} antialiased scroll-smooth`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,158 @@
+"use client"
+import { useEffect } from "react"
+import Link from "next/link"
+import { motion, stagger, useAnimate } from "motion/react"
+import Floating, { FloatingElement } from "@/components/ui/parallax-floating"
+import { ArrowRight } from "lucide-react"
+
+const exampleImages = [
+  {
+    url: "https://images.unsplash.com/photo-1727341554370-80e0fe9ad082?q=80&w=2276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "Branislav Rodman",
+    link: "https://unsplash.com/photos/a-black-and-white-photo-of-a-woman-brushing-her-teeth-r1SjnJL5tf0",
+    title: "A Black and White Photo of a Woman Brushing Her Teeth",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1640680608781-2e4199dd1579?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-painting-of-a-palm-leaf-on-a-multicolored-background-AaNPwrSNOFE",
+    title: "Neon Palm",
+    author: "Tim Mossholder",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1726083085160-feeb4e1e5b00?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-a-crowd-of-people-UgbxzloNGsc",
+    author: "ANDRII SOLOK",
+    title: "A blurry photo of a crowd of people",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1562016600-ece13e8ba570?q=80&w=2838&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/rippling-crystal-blue-water-9-OCsKoyQlk",
+    author: "Wesley Tingey",
+    title: "Rippling Crystal Blue Water",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1624344965199-ed40391d20f2?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/de/fotos/mann-im-schwarzen-hemd-unter-blauem-himmel-m8RDNiuEXro",
+    author: "Serhii Tyaglovsky",
+    title: "Mann im schwarzen Hemd unter blauem Himmel",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1689553079282-45df1b35741b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://unsplash.com/photos/a-woman-with-a-flower-crown-on-her-head-0S3muIttbsY",
+    author: "Vladimir Yelizarov",
+    title: "A women with a flower crown on her head",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1721968317938-cf8c60fccd1a?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "A blurry photo of white flowers in a field",
+    author: "Eugene Golovesov",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-white-flowers-in-a-field-6qbx0lzGPyc",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1677338354108-223e807fb1bd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "Mathilde Langevin",
+    link: "https://unsplash.com/photos/a-table-topped-with-two-wine-glasses-and-plates-Ig0gRAHspV0",
+    title: "A table topped with two wine glasses and plates",
+  },
+]
+
 export default function Home() {
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    animate("img", { opacity: [0, 1] }, { duration: 0.5, delay: stagger(0.15) })
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="w-full min-h-screen mx-auto mt-60" ref={scope}>
+      <motion.div
+        className="z-50 text-center space-y-4 items-center flex flex-col"
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.88, delay: 1.5 }}
+      >
+        <div className='flex gap-2 items-center bg-white backdrop-blur-lg px-6 py-1.5 rounded-full border border-neutral-300'>
+          <div className='bg-[#12C971] rounded-full w-2.5 h-2.5'></div>
+          <p className='text-sm opacity-70'>Servizio Disponibile</p>
+        </div>
+        <h1 className="text-6xl md:text-8xl z-50 font-serif mb-4 tracking-tight">
+          Porta l’esperienza del
+          <br/>
+          ristorante a casa tua.
+        </h1>
+        <p className="text-lg opacity-70 mb-6">
+          Prenota o scopri i servizi che ho da offrirti. 
+        </p>
+        <Link
+        href="/prenotazione"
+        className="group relative z-50 flex items-center text-left gap-4 w-40 px-4 py-2 bg-[var(--color-foreground)] text-white rounded-full hover:shadow-2xl duration-200">
+          Prenota Ora
+          <div className="absolute right-1 bg-white rounded-full p-2">
+          <ArrowRight className="text-[var(--color-foreground)] group-hover:-rotate-45 duration-200" size={16} />
+          </div>
+        </Link>
+      </motion.div>
+
+      <Floating sensitivity={-1} className="overflow-hidden">
+        <FloatingElement depth={0.5} className="top-[8%] left-[11%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[0].url}
+            className="w-16 h-16 md:w-24 md:h-24 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+        <FloatingElement depth={1} className="top-[10%] left-[32%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[1].url}
+            className="w-20 h-20 md:w-28 md:h-28 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+        <FloatingElement depth={2} className="top-[2%] left-[53%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[2].url}
+            className="w-28 h-40 md:w-40 md:h-52 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+        <FloatingElement depth={1} className="top-[0%] left-[83%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[3].url}
+            className="w-24 h-24 md:w-32 md:h-32 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+
+        <FloatingElement depth={1} className="top-[40%] left-[2%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[4].url}
+            className="w-28 h-28 md:w-36 md:h-36 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+        <FloatingElement depth={2} className="top-[70%] left-[77%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[7].url}
+            className="w-28 h-28 md:w-36 md:h-48 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+
+        <FloatingElement depth={4} className="top-[73%] left-[15%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[5].url}
+            className="w-40 md:w-52 h-full object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+        <FloatingElement depth={1} className="top-[80%] left-[50%]">
+          <motion.img
+            initial={{ opacity: 0 }}
+            src={exampleImages[6].url}
+            className="w-24 h-24 md:w-32 md:h-32 object-cover hover:scale-105 duration-200 cursor-pointer transition-transform"
+          />
+        </FloatingElement>
+      </Floating>
+    </div>
   )
 }
